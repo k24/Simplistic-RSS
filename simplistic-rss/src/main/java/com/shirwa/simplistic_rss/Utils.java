6c4ce8fa82e1b69c29588c1d151c72c69d168910
k24 <nishikei.9@gmail.com>
2016-03-11 01:37:03 +0900

    Fix parsing failures caused by the formatter without Locale

--- simplistic-rss/src/main/java/com/shirwa/simplistic_rss/Utils.java
+++ simplistic-rss/src/main/java/com/shirwa/simplistic_rss/Utils.java
@@ -6,31 +6,44 @@ import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.DateTimeFormatterBuilder;
 import org.joda.time.format.DateTimeParser;
 
+import java.util.Locale;
+
 public class Utils {
 
-    static final DateTimeFormatter FORMATTER = new DateTimeFormatterBuilder()
-            .append(null, new DateTimeParser[]{
-                    DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss'Z'").getParser(),
-                    DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").getParser(),
-                    DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ssZ").getParser(),
-                    DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ").getParser(),
-                    DateTimeFormat.forPattern("EEE, d MMM yy HH:mm:ss z").getParser(),
-                    DateTimeFormat.forPattern("EEE, d MMM yy HH:mm z").getParser(),
-                    DateTimeFormat.forPattern("EEE, d MMM yyyy HH:mm:ss z").getParser(),
-                    DateTimeFormat.forPattern("EEE, d MMM yyyy HH:mm:ss Z").getParser(),
-                    DateTimeFormat.forPattern("EEE, d MMM yyyy HH:mm z").getParser(),
-                    DateTimeFormat.forPattern("EEE d MMM yy HH:mm:ss z").getParser(),
-                    DateTimeFormat.forPattern("EEE d MMM yy HH:mm z").getParser(),
-                    DateTimeFormat.forPattern("EEE d MMM yyyy HH:mm:ss z").getParser(),
-                    DateTimeFormat.forPattern("EEE d MMM yyyy HH:mm:ss Z").getParser(),
-                    DateTimeFormat.forPattern("EEE d MMM yyyy HH:mm z").getParser(),
-                    DateTimeFormat.forPattern("d MMM yy HH:mm z").getParser(),
-                    DateTimeFormat.forPattern("d MMM yy HH:mm:ss z").getParser(),
-                    DateTimeFormat.forPattern("d MMM yyyy HH:mm z").getParser(),
-                    DateTimeFormat.forPattern("d MMM yyyy HH:mm:ss z").getParser(),
-            }).toFormatter();
+    static final DateTimeFormatter FORMATTER_DEFAULT;
+    static final DateTimeFormatter FORMATTER_US;
+
+    static {
+        DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+                .append(null, new DateTimeParser[]{
+                        DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss'Z'").getParser(),
+                        DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").getParser(),
+                        DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ssZ").getParser(),
+                        DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ").getParser(),
+                        DateTimeFormat.forPattern("EEE, d MMM yy HH:mm:ss z").getParser(),
+                        DateTimeFormat.forPattern("EEE, d MMM yy HH:mm z").getParser(),
+                        DateTimeFormat.forPattern("EEE, d MMM yyyy HH:mm:ss Z").getParser(),
+                        DateTimeFormat.forPattern("EEE, d MMM yyyy HH:mm:ss z").getParser(),
+                        DateTimeFormat.forPattern("EEE, d MMM yyyy HH:mm z").getParser(),
+                        DateTimeFormat.forPattern("EEE d MMM yy HH:mm:ss z").getParser(),
+                        DateTimeFormat.forPattern("EEE d MMM yy HH:mm z").getParser(),
+                        DateTimeFormat.forPattern("EEE d MMM yyyy HH:mm:ss Z").getParser(),
+                        DateTimeFormat.forPattern("EEE d MMM yyyy HH:mm:ss z").getParser(),
+                        DateTimeFormat.forPattern("EEE d MMM yyyy HH:mm z").getParser(),
+                        DateTimeFormat.forPattern("d MMM yy HH:mm z").getParser(),
+                        DateTimeFormat.forPattern("d MMM yy HH:mm:ss z").getParser(),
+                        DateTimeFormat.forPattern("d MMM yyyy HH:mm z").getParser(),
+                        DateTimeFormat.forPattern("d MMM yyyy HH:mm:ss z").getParser(),
+                }).toFormatter();
+        FORMATTER_US = formatter.withLocale(Locale.US);
+        FORMATTER_DEFAULT = formatter;
+    }
 
     static DateTime parseDate(String date) {
-        return FORMATTER.parseDateTime(date);
+        try {
+            return FORMATTER_US.parseDateTime(date);
+        } catch (IllegalArgumentException e) {
+            return FORMATTER_DEFAULT.parseDateTime(date);
+        }
     }
 }
